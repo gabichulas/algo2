762b97e975908f315dac8116833581bdbfad17b5
Gabriel Lopez Romero <gabriellopezromero@gmail.com>
2023-04-06 16:27:38 -0300

    insert de trie arreglado

--- practicas/tp-trie/code/trie.py
+++ practicas/tp-trie/code/trie.py
@@ -14,32 +14,33 @@ def insert(T,element):
         T.root.children.append(TrieNode())
         T.root.children[0].parent = T.root
         completeword(T.root.children[0],element,0)
-    end = False
-    i = 0
-    cont = 0
-    list = T.root.children
-    current = list[0]
-    while end != True:
-        if current.key == element[i]:
-            i +=1
-            if i == len(element):
-                current.isEndOfWord = True
-                return T
-            list = current.children
-            if not list:
-                current.children = []
-                current.children.append(TrieNode())
-                current.children[0].parent = current
+    else:
+        end = False
+        i = 0
+        cont = 0
+        list = T.root.children
+        current = list[0]
+        while end != True:
+            if current.key == element[i]:
+                i +=1
+                if i == len(element):
+                    current.isEndOfWord = True
+                    return T
+                list = current.children
+                if not list:
+                    current.children = []
+                    current.children.append(TrieNode())
+                    current.children[0].parent = current
+                    end = True
+                else:
+                    current = list[0]        
+            if current[cont+1] == None:
+                list.insert(0,TrieNode())
+                current[0].parent = current
                 end = True
             else:
-                current = list[0]        
-        if current[cont+1] == None:
-            list.insert(0,TrieNode())
-            current[0].parent = current
-            end = True
-        else:
-            cont += 1
-    completeword(list[0],element,i)
+                cont += 1
+        completeword(list[0],element,i)
     return T
 
 
@@ -48,7 +49,7 @@ def insert(T,element):
 def completeword(node,element,i):
     for i in range(i,len(element)-1):
         node.key = element[i]
-        node.childen = []
+        node.children = []
         node.children.append(TrieNode())
         node.children[0].parent = node
         node = node.children[0]
@@ -59,4 +60,4 @@ def completeword(node,element,i):
 arbol = Trie()
 insert(arbol,"rodrigo")
 
-print(arbol.root.children[0])
+print(arbol.root.children[0].key)
