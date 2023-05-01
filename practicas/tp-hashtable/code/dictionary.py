ee4d50910ce1a1f391ccf3f8dd7d07a4bc01b870
gabylopezromero13@gmail.com <gabylopezromero13@gmail.com>
2023-05-01 20:27:59 -0300

    updated hashtable

--- practicas/tp-hashtable/code/dictionary.py
+++ practicas/tp-hashtable/code/dictionary.py
@@ -2,9 +2,21 @@ class DictionaryElement:
     key = None
     value = None
 
+def createDictionaryString(D,string):
+    for ch in string:
+        insert(D,ord(ch),ch)
+    return D
+
+def areEqual(D,H):
+    if len(D) == len(H) and len(D) > 0:
+        for i in len(D):
+            if not (D[i].key == H[i].key and D[i].value.sort() == H[i].value.sort()):
+                return False
+        return True
+    return False
 
 def insert(D, key, value):
-    position = key % 9
+    position = key % 4
     newElement = DictionaryElement()
     newElement.key = key
     newElement.value = value
@@ -16,7 +28,7 @@ def insert(D, key, value):
 
 
 def search(D, key):
-    position = key % 9
+    position = key % 4
     element = D[position]
     if D[position] == None:
         return None
@@ -28,7 +40,7 @@ def search(D, key):
 
 
 def delete(D, key):
-    position = key % 9
+    position = key % 4
     element = D[position]
     if D[position] == None:
         return
@@ -40,3 +52,14 @@ def delete(D, key):
                 element.remove(nodo)
                 break
     return D
+
+
+def findPermutation(string,string2):
+    D = []
+    D2 = []
+    D = createDictionaryString(D,string)
+    D2 = createDictionaryString(D2,string2)
+    return areEqual(D,D2)
+
+
+
